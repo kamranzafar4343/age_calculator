ad020c1d01aa80679197c4be1bc0660dffef064f
Kamran Zafar <kamranzafar4343@gmail.com>
2024-02-09 23:14:02 +0500

    second commit

--- script.js
+++ script.js
@@ -22,13 +22,31 @@ function calculateMyAge(){
     let y2= today.getFullYear();  //stores today year
 
     //Calculating the difference
-
     let y3, m3, d3;
 
     //year difference
     y3= y2 - y1;
 
+    if(m2 >= m1){
+        m3= m2 - m1;
+    }
+    else{
+        y3--;
+        m3 = 12 + m2 - m1
+    }
     
-
-
+    if(d2 >= d1){
+        d3 = d2 - d1;
+    }
+    else{
+        m3--;
+        d3 = getDaysInMonth(y1, m1) + d2 - d1;   
+    }
+    if(m3 < 0){
+        m3 = 11;
+        y3--;
+    }
 }
+
+     
+    return new Date(year, month, 0).getDate();
